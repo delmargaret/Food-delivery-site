52140e4af9885354df1dcaede9f51f1eeb56bd39
Margaret <ri.tysik@mail.ru>
2020-03-08 19:37:29 +0300

    Add data context

---
+++ EzhaBy/EzhaBy.Infrastructure/DataContext/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using EzhaBy.Entities;
+using System.Data.Entity;
+
+namespace EzhaBy.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Cafe> Cafes { get; set; }
+        public DbSet<CafeDescription> CafeDescriptions { get; set; }
+        public DbSet<CafeSection> CafeSections { get; set; }
+        public DbSet<Complaint> Complaints { get; set; }
+        public DbSet<Courier> Couriers { get; set; }
+        public DbSet<CourierRequest> CourierRequests { get; set; }
+        public DbSet<DescriptionKeyword> KeyWords { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+        public DbSet<PartnerRequest> PartnerRequests { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        static DataContext()
+        {
+            Database.SetInitializer(new ProjectDbInitializer());
+        }
+
+        public DataContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+
+    public class ProjectDbInitializer : DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext db)
+        {
+            
+        }
+    }
+}
