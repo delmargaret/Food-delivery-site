5fee7db458ed6fad304341a59a5a0e03cc118722
Margarita Delikatnaya <rita.delikatnaya@mail.ru>
2020-04-07 23:23:50 +0300

    Add tags validation

--- EzhaBy/EzhaBy.Web/ezha-by-admin/src/components/tags/add-tag-form.js
+++ EzhaBy/EzhaBy.Web/ezha-by-admin/src/components/tags/add-tag-form.js
@@ -1,38 +1,64 @@
-import React, { Component } from 'react';
-import { Button, Form, Col, Row } from 'react-bootstrap';
-import TagsService from '../../services/tags-service';
+import React, { Component } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import TagsService from "../../services/tags-service";
 
 export default class AddTagForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      validated: false
+    };
 
     this.tagNameInput = React.createRef();
-    this.onTagAddHandler = this.onTagAdd.bind(this);
+    this.onTagAdd = this.onTagAdd.bind(this);
   }
 
-  onTagAdd() {
-    TagsService.createTag(this.tagNameInput.current.value);
-    this.tagNameInput.current.value = null;
+  async onTagAdd(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const form = event.currentTarget;
+    if (form.checkValidity()) {
+      await TagsService.createTag(this.tagNameInput.current.value);
+    }
+
+    this.setState({
+      validated: true
+    });
   }
 
   render() {
+    const { validated } = this.state;
+
     return (
-      <div>
+      <React.Fragment>
         <br />
-        <Form>
-          <Form.Label><b>Новый тэг</b></Form.Label>
-          <Form.Group as={Row} controlId="tagName">
+        <Form noValidate validated={validated} onSubmit={this.onTagAdd}>
+          <Form.Label>
+            <b>Новый тэг</b>
+          </Form.Label>
+          <Row>
             <Col sm="4">
-              <Form.Control
-                ref={this.tagNameInput}
-                type="text"
-                placeholder="Название тэга"
-              />
+              <Form.Group controlId="tagName">
+                <Form.Control
+                  ref={this.tagNameInput}
+                  type="text"
+                  placeholder="Название тэга"
+                  required
+                />
+                <Form.Control.Feedback type="invalid">
+                  Введите название тэга
+                </Form.Control.Feedback>
+              </Form.Group>
             </Col>
-            <Button className="btn-red" onClick={this.onTagAddHandler}>Добавить</Button>
-          </Form.Group>
+            <Col>
+              <Button className="btn-red" type="submit">
+                Добавить
+              </Button>
+            </Col>
+          </Row>
         </Form>
-      </div>
+      </React.Fragment>
     );
   }
 }
