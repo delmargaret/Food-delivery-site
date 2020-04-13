cafe19aabbf72641f8d1d3a4f6ec7f9b6d4b749f
Margarita Delikatnaya <m.delikatnaya@itransition.com>
2020-04-13 21:06:31 +0300

    Fix position of footer

--- EzhaBy/EzhaBy.Web/ezha-by-admin/src/components/footer/footer.js
+++ EzhaBy/EzhaBy.Web/ezha-by-admin/src/components/footer/footer.js
@@ -4,7 +4,7 @@ import { Navbar } from "react-bootstrap";
 export default class FooterComponent extends Component {
   render() {
     return (
-      <Navbar sticky="bottom" bg="light">
+      <Navbar fixed="bottom" bg="light" className="footer">
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>Ezha.by@{new Date().getFullYear()}</Navbar.Text>
         </Navbar.Collapse>
