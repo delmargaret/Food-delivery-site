e0f59436c7dc7950d1d77c1c44905ddf378aa706
Margarita Delikatnaya <rita.delikatnaya@mail.ru>
2020-04-18 10:26:46 +0300

    Fix footer alignment

--- EzhaBy/EzhaBy.Web/ezha-by-admin/src/components/footer/footer.js
+++ EzhaBy/EzhaBy.Web/ezha-by-admin/src/components/footer/footer.js
@@ -4,7 +4,7 @@ import { Navbar } from "react-bootstrap";
 export default class FooterComponent extends Component {
   render() {
     return (
-      <Navbar fixed="bottom" bg="light" className="footer">
+      <Navbar bg="light" className="footer">
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>Ezha.by@{new Date().getFullYear()}</Navbar.Text>
         </Navbar.Collapse>
