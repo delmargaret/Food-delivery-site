81e4afb13b2a1c185c1692be42c8e0045ec8b29b
Margarita Delikatnaya <m.delikatnaya@itransition.com>
2020-04-18 16:58:23 +0300

    Add 'UnitTests' project

---
+++ EzhaBy/EzhaBy.UnitTests/CQTests/TagsTests.cs
@@ -0,0 +1,41 @@
+﻿using EzhaBy.Business.Tags;
+using EzhaBy.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EzhaBy.UnitTests.CQTests
+{
+    [TestClass]
+    public class TagsTests
+    {
+        private DataContext context;
+
+        public TagsTests()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "Db")
+                .Options;
+
+            context = new DataContext(options);
+        }
+
+        [TestMethod]
+        public void CanCreateTagAsync()
+        {
+            var tagName = "tag";
+
+            var handler = new CreateTag.Handler(context);
+            var command = new CreateTag.Command(tagName);
+
+            var cancellationToken = new CancellationToken();
+
+            _ = handler.Handle(command, cancellationToken);
+
+            var expectedResult = context.Tags.FirstOrDefaultAsync(tag => tag.TagName == tagName);
+
+            Assert.IsNotNull(expectedResult);
+        }
+    }
+}
